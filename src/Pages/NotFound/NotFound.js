f3736f1309a19015613a4e45f64481fce8b54714
Khurshida-Meem <meemrumi59@gmail.com>
2021-11-19 23:08:26 +0600

    fixed margin

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
+        <div style={{ textAlign: 'center' }}>
             <h1> 404 not found!</h1>
         </div>
     );
