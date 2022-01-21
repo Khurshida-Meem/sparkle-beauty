a76ea08c5654e56a51bfb675f79fcc1a79b16440
Khurshida-Meem <meemrumi59@gmail.com>
2022-01-21 23:46:03 +0600

    added slider on review

--- src/Pages/Home/Reviews/Review.js
+++ src/Pages/Home/Reviews/Review.js
@@ -12,28 +12,27 @@ const Review = ({ review }) => {
     }
     return (
 
-        <Grid item xs={12} sm={6} md={4}>
-            <Card sx={{ maxWidth: 345 }}>
-                <Box sx={{ mt: 2 }} style={{ display: 'flex', justifyContent: 'center' }}>
-                    <Avatar
-                        sx={{ bgcolor: deepOrange[500] }}
-                        alt={userName}
-                        src={thumb}
-                    />
+
+        <Card sx={{ maxWidth: 345 }}>
+            <Box sx={{ mt: 2 }} style={{ display: 'flex', justifyContent: 'center' }}>
+                <Avatar
+                    sx={{ bgcolor: deepOrange[500] }}
+                    alt={userName}
+                    src={thumb}
+                />
+            </Box>
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div" style={{ textAlign: 'center' }}>
+                    {userName}
+                </Typography>
+                <Box style={{ textAlign: 'center' }}>
+                    <Rating name="rating" value={star} readOnly />
                 </Box>
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div" style={{ textAlign: 'center' }}>
-                        {userName}
-                    </Typography>
-                    <Box style={{ textAlign: 'center' }}>
-                        <Rating name="rating" value={star} readOnly />
-                    </Box>
-                    <Typography variant="body2" color="text.secondary" style={{ textAlign: 'center' }}>
-                        {description}
-                    </Typography>
-                </CardContent>
-            </Card>
-        </Grid>
+                <Typography variant="body2" color="text.secondary" style={{ textAlign: 'center' }}>
+                    {description}
+                </Typography>
+            </CardContent>
+        </Card>
     );
 };
 
--- src/Pages/Home/Reviews/Reviews.js
+++ src/Pages/Home/Reviews/Reviews.js
@@ -3,6 +3,9 @@ import { Container, Grid } from '@mui/material';
 import { Box } from '@mui/system';
 import useData from '../../../hooks/useData'
 import Review from './Review';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const contentContainer = {
     marginTop: '100px'
@@ -12,13 +15,31 @@ const Reviews = () => {
 
     const reviews = useData('https://sparkle-beauty.herokuapp.com/reviews');
 
+    let settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        autoplay: true,
+        responsive: [
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
+
     return (
         <Container style={contentContainer} sx={{ flexGrow: 1 }}>
             <Box sx={{ fontSize: '24px', m: 1, fontWeight: 'bold', textAlign: 'center' }}>Featured Reviews</Box>
             <Box sx={{ fontSize: '32px', m: 1, fontWeight: 'bold', textAlign: 'center', color: '#ff8ba7', mb: 2 }}>Thoughts of Our Well-Wishers</Box>
 
 
-            <Grid container spacing={2}>
+            <Slider {...settings}>
                 {
                     reviews.map(review => <Review
                         key={review._id}
@@ -26,7 +47,7 @@ const Reviews = () => {
                     >
                     </Review>)
                 }
-            </Grid>
+            </Slider>
 
         </Container>
 
